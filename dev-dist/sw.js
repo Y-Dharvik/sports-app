b301e5d8e929f65483aa9135ca65d8aebf0089fb
Y-Dharvik <21951a0536@iare.ac.in>
2024-09-20 00:02:12 +0530

    update the en.json translations

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.smc953j087g"
+    "revision": "0.dmo3pu231q"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
